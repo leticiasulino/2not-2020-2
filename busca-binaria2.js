ee38df9041cd9b9c90a4ff57dc4420847250e96f
leticiasulino <leticiasuli1@hotmail.com>
2020-10-24 22:15:03 +0000

    24/10

--- busca-binaria2.js
+++ busca-binaria2.js
@@ -1,28 +1,26 @@
 //Pré-requisito para a busca binária: o conjunto de dados PRECISA estar ordenados pelo critério de busca
 
 let comp = 0
-function buscaBinaria(lista, valorBusca){
+function buscaBinaria(lista, valorBusca, fnComp){
     let inicio = 0 
     let fim = lista.lenght - 1
 
     while(fim >= inicio){
 
         let meio = Math.floor((fim + inicio)) / 2
-        //o inicio, o fim e o meio pode mudar de lugar
-        //Math.floor: arredonda para baixo
-        //Math.ceil: arrendoda para cima
-        //Math.arround: arrendoda para cima
+
+        let res = fnComp(lista[meio], valorBusca)
 
         //Verificar se o valor na posição média é o valor de busca
-        if(valorBusca === lista[meio]) {
+        if(res == 0) {
             comp++
             return meio
         }
-        else if(valorBusca < lista[meio]){
+        else if(res < 0){
             comp += 2
             fim = meio - 1
         }
-        else{   //valorBusca > lista[meio]
+        else{   // res > 0
             comp += 2
             inicio = meio + 1
         }
@@ -31,6 +29,15 @@ function buscaBinaria(lista, valorBusca){
 
 }
 
+function comparaNome(obj, valorBusca){
+    //Os dois valores são iguais
+    if(valorBusca === obj.first_name) return 0
+    //Um número negativo para indicar que o primeiro < segundo
+    else if(valorBusca < obj.first_name) return -1
+    //Um número positivo para indicar que o primeiro > segundo
+    else return 1 // valorBusca > obj.fist_name
+    
+}
 
 let vetorNomes = require('./dados/vetor-nomes')
 
